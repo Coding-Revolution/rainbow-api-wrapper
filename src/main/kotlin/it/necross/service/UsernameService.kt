e14a14424300227e17abdbc1bf80f7e15b7eeaf5
Kelvin Bill <2foralp@googlemail.com>
2021-12-09 23:05:27 +0100

    feature: Add username service

---
+++ src/main/kotlin/it/necross/service/UsernameService.kt
@@ -0,0 +1,18 @@
+package it.necross.service
+
+import it.necross.RainbowWrapper
+import it.necross.connection.ConnectionBuilder
+import it.necross.enums.GamePlatform
+import it.necross.enums.RestType
+import it.necross.interfaces.username.IUsername
+import it.necross.util.Data
+
+class UsernameService(private val connectionBuilder: ConnectionBuilder) {
+
+    fun getUserName(platform: GamePlatform, userId: String): IUsername {
+        val result = connectionBuilder.makeRequest("${Data.apiUrl}/${platform.identifier}/username/$userId", RestType.GET)
+        val jsonData: String = result.body()!!.string()
+
+        return RainbowWrapper.gson.fromJson(jsonData, IUsername::class.java)
+    }
+}
